ccdd65d0f3ae38631adc221f6964fdc87631e455
Rajesh Kumar <rajesh.kumar@infracloud.io>
2022-05-27 11:41:23 +0530

    Updated ports

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def index():
     return render_template('index.html', speedtests = data)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=3120)
+    app.run(debug=True,host='0.0.0.0',port=5000)
